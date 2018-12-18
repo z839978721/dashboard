ab5a515d7b2e48b3955cd38ee41c5caf64b13202
zlq <839978721@qq.com>
2018-12-18 09:57:51 +0800

    user

--- src/main/java/com/cloud/app/user/UserController.java
+++ src/main/java/com/cloud/app/user/UserController.java
@@ -6,13 +6,13 @@ import com.cloud.aspect.ResultUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(value = {"/user"})
 public class UserController {
 
