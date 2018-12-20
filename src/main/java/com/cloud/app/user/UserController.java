7749c070a928011a782fc9a0a1b5300950ce2347
zlq <839978721@qq.com>
2018-12-20 17:10:28 +0800

    session

--- src/main/java/com/cloud/app/user/UserController.java
+++ src/main/java/com/cloud/app/user/UserController.java
@@ -1,18 +1,23 @@
 package com.cloud.app.user;
 
-import com.cloud.aspect.Result;
-import com.cloud.aspect.ResultHandle;
-import com.cloud.aspect.ResultUtil;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.cloud.aspect.Result;
+import com.cloud.aspect.ResultHandle;
+import com.cloud.aspect.ResultUtil;
 
-@Controller
+@RestController
 @RequestMapping(value = {"/user"})
 public class UserController {
 
@@ -31,4 +36,5 @@ public class UserController {
         result = ResultUtil.success(list);
         return result;
     }
+    
 }
